0787c4682b2263b24c942813ade2814c32f86823
rdoddanavar <roshandoddanavar@gmail.com>
2019-04-20 20:48:59 -0500

    run script

--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
-placeholder
+#!/usr/bin/env bash
+
+EXEDIR="/home/${USER}/Git/project-euler/bin/"
+PREFIX="problem"
+
+NUM="$1" # Program input
+make all # Build 
+
+EXEDIR+=$PREFIX # Build path
+EXEDIR+=$NUM
+
+$EXEDIR # Execute
